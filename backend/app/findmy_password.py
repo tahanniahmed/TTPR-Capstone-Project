f780995310ff73e8f169216d958ceb765adcccb1
Michael Schlichting Barbey <michael.schlichtingbarbey@baruchmail.cuny.edu>
2025-08-17 15:50:32 -0400

    Refactor password finder and update password list
    Refactored findmy_password.py to use dynamic file paths and improved password encoding logic. Updated passwords.txt to contain only a single password entry, reducing file size and removing the previous large list.

--- backend/app/findmy_password.py
+++ backend/app/findmy_password.py
@@ -1,17 +1,24 @@
 import json
 import base64
 import requests
+import os
 
 def finder():
     # to maintain the integrity of the password, we will obfuscate it using base64 encoding for this project.
     # this is only to demostrate the concept of obfuscation and should not be used in production code, we only feed the actual password to the program because it is not part of the list in order to protect access to the VM. 
+    
+    # file directory
+    base_dir = os.path.dirname(__file__)  # directory of app/__init__.py
+    passKey = os.path.join(base_dir, "target_ip.json")
+    
     actual_password = input("PASSWORD: ")
-    with open('password_key.json', 'w') as pass_key:
+    with open(passKey, 'w') as pass_key:
         # encode (obfuscate)
-        actual_password = base64.b64encode(actual_password.encode()).decode()
-        print("Encoded:", actual_password)
-        pass_data = pass_key.write(json.dumps({"pass_key": actual_password}))
-
+        encoded_pass = base64.b64encode(actual_password.encode()).decode()
+        pass_data = json.load(pass_key)
+        pass_data["pass_key"] = encoded_pass
+        print("Encoded:", encoded_pass)
+        
     # web scraping the password list from KoreLogic's SecLists repository
     # this is a list of common passwords that can be used for testing purposes.
     url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/refs/heads/master/Passwords/Permutations/korelogic-password.txt'
@@ -20,7 +27,7 @@ def finder():
     passwords = data.text.splitlines()
 
     for password in passwords:
-        with open('passwords.txt', 'a') as password_file:
+        with open('passwords.txt', 'w') as password_file:
             password_file.write(password + '\n')
             
     with open('password_key.json', 'r') as pass_key:
